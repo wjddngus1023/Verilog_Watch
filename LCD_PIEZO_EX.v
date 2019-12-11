db7284274ef6069b31b776f8e4def1267b9ceb10
wjddngus1023 <46593313+wjddngus1023@users.noreply.github.com>
2019-12-11 15:07:40 +0900

    2019.12.11 파이널프로젝트 수정
    1차기능 - 시간수정가능

--- LCD_PIEZO_EX.v
+++ LCD_PIEZO_EX.v
@@ -2,12 +2,10 @@
 
 module LCD_PIEZO_EX(
 	RESETN,CLK,
-	KEY,
 	PIEZO
 );
 
 input RESETN,CLK;
-input [7:0] KEY;
 output PIEZO;
 
 reg BUFF;
@@ -16,20 +14,7 @@ integer LIMIT;
 
 wire PIEZO;
 
-always @(KEY)
-begin
-		case(KEY)
-			8'b10000000 : LIMIT = 190; //C
-			8'b01000000 : LIMIT = 169; //D
-			8'b00100000 : LIMIT = 151; //E
-			8'b00010000 : LIMIT = 142; //F
-			8'b00001000 : LIMIT = 127; //G
-			8'b00000100 : LIMIT = 113; //A
-			8'b00000010 : LIMIT = 100; //B
-			8'b00000001 : LIMIT = 95; //HIGH C
-			default : LIMIT = 0;
-		endcase
-	end
+
 
 	always @(posedge CLK)
 	begin
--- LCD_WATCH.v
+++ LCD_WATCH.v
@@ -1,7 +1,7 @@
-
 module LCD_WATCH(
 	RESETN,CLK,
-	H10,H1,M10,M1,S10,S1
+	H10,H1,M10,M1,S10,S1,
+	KEY1,KEY2,KEY3,KEY4,KEY5,KEY6,KEY7,KEY8
 	
 );
 
@@ -11,15 +11,15 @@ module LCD_WATCH(
 
 
 input RESETN,CLK;
-wire [3:0]H10,H1,M10,M1,S10,S1;
+wire [3:0] H10,H1,M10,M1,S10,S1;
 integer CNT;
 integer CNT_SCAN;
 
-reg[6:0]HOUR,MIN,SEC;
-output [3:0]H10,H1,M10,M1,S10,S1;
-reg[3:0]NUM;
+input  KEY1,KEY2,KEY3,KEY4,KEY5,KEY6,KEY7,KEY8;
 
-
+reg[6:0] HOUR,MIN,SEC;
+output [3:0] H10,H1,M10,M1,S10,S1;
+reg[3:0] NUM;
 
 always @(posedge CLK)
 begin  // CNT 생성
@@ -27,7 +27,7 @@ begin  // CNT 생성
 		CNT=0;
 	else
 		begin
-			if(CNT>=999)
+			if(CNT>=99999)
 				CNT=0;
 			else
 				CNT=CNT+1;
@@ -38,9 +38,15 @@ always @(posedge CLK)
 begin	// SEC연산
 	if(~RESETN)
 		SEC=0;
-	else
+	else 
 		begin
-			if(CNT==999)
+			if(KEY5 && CNT == 99999)
+				SEC = SEC + 1;
+			if(KEY6 && CNT == 99999)
+				SEC = SEC - 1;
+		else 
+			begin
+				if(CNT==99999)
 				begin
 					if(SEC>=59)
 						SEC=0;
@@ -49,14 +55,21 @@ begin	// SEC연산
 				end
 		end
 end
+end
 
 always @(posedge CLK)
 begin  // MIN 연산
 	if(~RESETN)
 		MIN=0;
-	else
+		else 
 		begin
-			if((CNT==999)&&(SEC==59))
+			if(KEY3 && CNT == 99999)
+				MIN = MIN + 1;
+			if(KEY4 && CNT == 99999)
+				MIN = MIN - 1;
+		else 
+		begin
+			if((CNT==99999)&&(SEC==59))
 				begin
 					if(MIN>=59)
 						MIN=0;
@@ -65,14 +78,20 @@ begin  // MIN 연산
 				end
 		end
 end
-
+end
 always @(posedge CLK)
 begin  // HOUR연산
 	if(~RESETN)
 		HOUR=0;
-	else
+		else 
 		begin
-			if((CNT==999)&&(SEC==59)&&(MIN==59))
+			if(KEY1 && CNT == 99999)
+				HOUR = HOUR + 1;
+			if(KEY2 && CNT == 99999)
+				HOUR = HOUR - 1;
+		else 
+		begin
+			if((CNT==99999)&&(SEC==59)&&(MIN==59))
 				begin
 					if(HOUR>=23)
 						HOUR=0;
@@ -81,12 +100,11 @@ begin  // HOUR연산
 				end
 		end
 end
-
+end
 
 LCD_WATCH_SEP S_SEP(SEC,S10,S1);
 LCD_WATCH_SEP M_SEP(MIN,M10,M1);
 LCD_WATCH_SEP H_SEP(HOUR,H10,H1);
-
 
 
 endmodule
